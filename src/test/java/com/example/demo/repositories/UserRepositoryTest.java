00962e7f092bdc2c40237d1cae7cf473fd096812
belu <belen.amat29@gmail.com>
2020-06-20 12:14:14 -0300

    update de los datos de un usuario en Repositorios

--- src/test/java/com/example/demo/repositories/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repositories/UserRepositoryTest.java
@@ -1,16 +1,18 @@
 package com.example.demo.repositories;
 
 import com.example.demo.builders.ClientUserBuilder;
+import com.example.demo.builders.StoreAdminBuilder;
 import com.example.demo.model.user.ClientUser;
+import com.example.demo.model.user.StoreAdminUser;
 import com.example.demo.model.user.User;
 import com.example.demo.repositories.threshold.MoneyThresholdRepository;
 import com.example.demo.repositories.users.UserRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 
@@ -35,4 +37,34 @@ public class UserRepositoryTest {
         assertThat(retrievedUser).hasValue(user);
     }
 
+    @Test
+    public void aUserIsUpdated() {
+        ClientUser user = ClientUserBuilder.user().withAddress("Alsina 123").withPassword("ABC789").build();
+        User savedUser = userRepository.save(user);
+
+        User retrievedUser = userRepository.findById(savedUser.id()).get();
+        retrievedUser.setPassword("newPassword");
+        retrievedUser.setAddress("Alvear 4512");
+
+        userRepository.save(retrievedUser);
+        retrievedUser = userRepository.findById(retrievedUser.id()).get();
+
+        assertEquals(retrievedUser.password(), "newPassword");
+        assertEquals(retrievedUser.address(), "Alvear 4512");
+    }
+
+    @Test
+    public void aStoreAdminUserCanUpdateItsPassword() {
+        StoreAdminUser storeAdminUser = StoreAdminBuilder.aStoreAdmin().withPassword("ABC789").build();
+        User savedUser = userRepository.save(storeAdminUser);
+
+        User retrievedUser = userRepository.findById(savedUser.id()).get();
+        retrievedUser.setPassword("newPassword");
+
+        userRepository.save(retrievedUser);
+        userRepository.save(retrievedUser);
+        retrievedUser = userRepository.findById(retrievedUser.id()).get();
+
+        assertEquals(retrievedUser.password(), "newPassword");
+    }
 }
